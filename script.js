ad9d02fd18a48d14a5880afa41c13fbcf2926941
hardikk04 <hardik04062003@gmail.com>
2023-10-31 00:16:15 +0530

    page1 complete

--- script.js
+++ script.js
@@ -115,3 +115,11 @@ t1.to(".middle-right", {
     // markers: true,
   },
 });
+
+Shery.mouseFollower();
+
+Shery.makeMagnet(".magnet" /* Element to target.*/, {
+  //Parameters are optional.
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
