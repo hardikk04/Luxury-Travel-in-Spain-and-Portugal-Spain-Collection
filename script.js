7c3e893811bbb0b6a8c71b7e7bc917e4201b7150
hardikk04 <hardik04062003@gmail.com>
2023-10-31 22:12:25 +0530

    only footer left

--- script.js
+++ script.js
@@ -256,3 +256,12 @@ cut.addEventListener("click", () => {
     zIndex: "-100",
   });
 });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
